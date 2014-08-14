7425839c7496f7501281ad23855f14f38ecbba07
Dominic May <jack.thatch@gmail.com>
2014-08-14 11:38:24 +0800

    Reverse order of sort resulting from VisitComparator

--- src/com/lysdev/transperthcached/timetable/VisitComparator.java
+++ src/com/lysdev/transperthcached/timetable/VisitComparator.java
@@ -4,8 +4,8 @@ import java.util.Comparator;
 
 public class VisitComparator implements Comparator<Visit> {
     public int compare(Visit v1, Visit v2) {
-        if      (v1.time.isBefore(v2.time)) return  1;
-        else if (v1.time.isAfter(v2.time))  return -1;
-        else                           return  0;
+        if      (v1.time.isBefore(v2.time)) return -1;
+        else if (v1.time.isAfter(v2.time))  return  1;
+        else                                return  0;
     }
 }
