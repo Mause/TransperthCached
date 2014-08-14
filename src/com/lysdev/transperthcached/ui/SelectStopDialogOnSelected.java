20ce8a1c5d41b8664e29d27cbab3672083418b0e
Dominic May <jack.thatch@gmail.com>
2014-08-14 11:34:03 +0800

    Code for stop selection by the user (buggy as hell)

---
+++ src/com/lysdev/transperthcached/ui/SelectStopDialogOnSelected.java
@@ -0,0 +1,7 @@
+package com.lysdev.transperthcached.ui;
+
+import com.lysdev.transperthcached.silverrails.NearbyTransitStop;
+
+public abstract class SelectStopDialogOnSelected {
+    abstract public void onSelected(NearbyTransitStop nearby_stop);
+}
