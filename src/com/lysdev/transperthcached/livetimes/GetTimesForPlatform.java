352759cb43772eda8fbf72be62ba93bfdf7f4f42
Dominic May <jack.thatch@gmail.com>
2014-08-16 15:47:54 +0800

    Cleanup

--- src/com/lysdev/transperthcached/livetimes/GetTimesForPlatform.java
+++ src/com/lysdev/transperthcached/livetimes/GetTimesForPlatform.java
@@ -1,25 +1,13 @@
 package com.lysdev.transperthcached.livetimes;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import android.util.Log;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeFormat;
 
 public class GetTimesForPlatform {
 
