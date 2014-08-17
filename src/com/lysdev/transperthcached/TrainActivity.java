871fe6c645a700ff6186471fc2379448ba611c6a
Dominic May <jack.thatch@gmail.com>
2014-08-17 12:05:30 +0800

    Empty tab

---
+++ src/com/lysdev/transperthcached/TrainActivity.java
@@ -0,0 +1,15 @@
+package com.lysdev.transperthcached;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TrainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textview = new TextView(this);
+        textview.setText("This is the train tab");
+        setContentView(textview);
+    }
+}
