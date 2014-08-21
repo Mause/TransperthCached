c5674e4ad08d92954440a912fd2ac47b379d1ec4
Dominic May <jack.thatch@gmail.com>
2014-08-21 22:06:19 +0800

    Move database specific logic into DatabaseHelper && cleanup

--- src/com/lysdev/transperthcached/timetable/DatabaseHelper.java
+++ src/com/lysdev/transperthcached/timetable/DatabaseHelper.java
@@ -5,8 +5,11 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Vector;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
@@ -16,11 +19,10 @@ import android.util.Log;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    // private static String DB_PATH = "/data/data/com.lysdev.transperthcached/databases/";
     private static String DB_PATH = "/sdcard/Android/data/com.lysdev.transperthcached/databases/";
     private static String DB_NAME = "transperthcache.db";
 
-    private SQLiteDatabase myDataBase;
+    private SQLiteDatabase db;
 
     private final Context myContext;
 
@@ -68,14 +70,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return checkDB != null;
     }
 
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-
     private void copyDataBase() throws IOException {
         // Open your local db as the input stream
         InputStream myInput = myContext.getAssets().open(DB_NAME);
@@ -109,17 +103,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public SQLiteDatabase openDataBase() throws SQLException {
         String myPath = DB_PATH + DB_NAME;
 
-        myDataBase = SQLiteDatabase.openDatabase(
+        db = SQLiteDatabase.openDatabase(
             myPath, null, SQLiteDatabase.OPEN_READONLY
         );
 
-        return myDataBase;
+        return db;
     }
 
     @Override
     public synchronized void close() {
-        if(myDataBase != null) {
-            myDataBase.close();
+        if(db != null) {
+            db.close();
         }
 
         super.close();
@@ -132,6 +126,127 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
 
     // Add your public helper methods to access and get content from the database.
-    // You could return cursors by doing "return myDataBase.query(....)" so it'd be easy
+    // You could return cursors by doing "return db.query(....)" so it'd be easy
     // to you to create adapters for your views.
+
+    private Visit build_result(Cursor cursor) {
+        String stop_num, time;
+        int route_num, time_hour, time_minute;
+
+        time_hour = cursor.getInt(3);
+        time_minute = cursor.getInt(4);
+
+        stop_num = cursor.getString(0);
+        route_num = cursor.getInt(2);
+
+        return Visit.fromRaw(
+            stop_num,
+            route_num,
+            time_hour,
+            time_minute
+        );
+    }
+
+    public StopTimetable getVisitsForStop(String stop_num) {
+        Log.d("TransperthCached", "getting results from db");
+
+        // SQLiteDatabase db = getReadableDatabase();
+
+        Log.d("TransperthCached", "DB Path: " + db.getPath());
+
+        assert ((
+            "/sdcard/Android/data/" +
+            "com.lysdev.transperthcached/databases" +
+            "/transperthcache.db").equals(db.getPath())
+        );
+
+        Cursor cursor = db.query(
+            "visit",              // table
+            new String[] { "*" }, // selected fields
+            "stop_num=?",         // where clause
+            new String[] { String.valueOf(stop_num) },  // where fields
+            null, // having
+            null, // orderBy
+            null  // limit
+        );
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        } else {
+            return null;
+        }
+
+        Vector<Visit> weekdays = new Vector<Visit>(),
+                      saturday = new Vector<Visit>(),
+                      sunday   = new Vector<Visit>();
+
+        int num_results = 0;
+        while (!cursor.isAfterLast()) {
+            int visit_day_type = cursor.getInt(1);
+            Visit result = build_result(cursor);
+
+            switch(visit_day_type) {
+                case Timetable.WEEKDAYS:  weekdays.add(result);   break;
+                case Timetable.SATURDAY:  saturday.add(result);   break;
+                case Timetable.SUNDAY:    sunday.add(result);     break;
+                default:        throw new java.lang.Error("Bad visit type");
+            }
+            num_results++;
+            cursor.moveToNext();
+        }
+        Log.d("TransperthCached", num_results + " results");
+
+        if (num_results == 0) {
+            // ie, no such stop
+            return null;
+        }
+
+        return new StopTimetable(
+            stop_num,
+
+            weekdays,
+            saturday,
+            sunday
+        );
+    }
+
+    public String getDescriptionOfStop(String stop_number) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.query(
+            "stops",
+            new String[] { "Description" },
+            "Code=?",
+            new String[] { stop_number },
+            null, null, null
+        );
+
+        if (cursor == null) return null;
+
+        cursor.moveToFirst();
+        return cursor.getString(0);
+    }
+
+    public ArrayList<String> listTables() {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.query(
+            "sqlite_master",
+            new String[] { "name" },
+            "type=?",
+            new String[] { "table" },
+            null, null, null
+        );
+
+        if (cursor == null) return null;
+
+        cursor.moveToFirst();
+
+        ArrayList<String> tables = new ArrayList<String>();
+        while (!cursor.isAfterLast()) {
+            tables.add(cursor.getString(0));
+        }
+
+        return tables;
+    }
 }
--- src/com/lysdev/transperthcached/timetable/Timetable.java
+++ src/com/lysdev/transperthcached/timetable/Timetable.java
@@ -1,49 +1,42 @@
 package com.lysdev.transperthcached.timetable;
 
 import java.util.HashMap;
-import java.util.Vector;
 import java.io.IOException;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 
 public class Timetable {
-    private static final int WEEKDAYS = 0x1;
-    private static final int SATURDAY = 0x2;
-    private static final int SUNDAY   = 0x3;
+    public static final int WEEKDAYS = 0x1;
+    public static final int SATURDAY = 0x2;
+    public static final int SUNDAY   = 0x3;
 
     private HashMap<String, StopTimetable> _cache;
-    private SQLiteDatabase db;
+    private DatabaseHelper helper;
 
     public Timetable() {
         _cache = new HashMap<String, StopTimetable>();
     }
 
     public void onDestroy() {
-        this.db.close();
+        if (this.helper != null)
+            this.helper.close();
     }
 
     public void initialize(Context context) throws IOException {
-        DatabaseHelper helper = new DatabaseHelper(context);
+        helper = new DatabaseHelper(context);
 
-        // try {
             helper.createDataBase();
-        // } catch (IOException ioe) {
-        //     throw new Error("Unable to create database");
-        // }
 
         try {
-            this.db = helper.openDataBase();
+            helper.openDataBase();
         } catch (SQLException sqle) {
             Log.d("TransperthCached", "Caught SQLException");
             throw sqle;
         }
-
-        Log.d("TransperthCached", "Its open? "  + db.isOpen());
     }
 
     public StopTimetable getVisitsForStop(String stop_num) {
@@ -57,78 +50,11 @@ public class Timetable {
         return _getVisitsForStop_uncached(stop_num);
     }
 
-    private Visit build_result(Cursor cursor) {
-        String stop_num, time;
-        int route_num, time_hour, time_minute;
-
-        time_hour = cursor.getInt(3);
-        time_minute = cursor.getInt(4);
-
-        stop_num = cursor.getString(0);
-        route_num = cursor.getInt(2);
-
-        return Visit.fromRaw(
-            stop_num,
-            route_num,
-            time_hour,
-            time_minute
-        );
-    }
-
     private StopTimetable _getVisitsForStop_uncached(String stop_num) {
-        Log.d("TransperthCached", "getting results from db");
+        StopTimetable st = helper.getVisitsForStop(stop_num);
 
-        if (db == null)
-            Log.d("TransperthCached", "Bad database");
+        if (st == null) return st;
 
-        Cursor cursor = db.query(
-            "visit",              // table
-            new String[] { "*" }, // selected fields
-            "stop_num=?",         // where clause
-            new String[] { String.valueOf(stop_num) },  // where fields
-            null, // having
-            null, // orderBy
-            null  // limit
-        );
-
-        if (cursor != null) {
-            cursor.moveToFirst();
-        } else {
-            return null;
-        }
-
-        Vector<Visit> weekdays = new Vector<Visit>(),
-                      saturday = new Vector<Visit>(),
-                      sunday   = new Vector<Visit>();
-
-        int num_results = 0;
-        while (!cursor.isAfterLast()) {
-            int visit_day_type = cursor.getInt(1);
-            Visit result = build_result(cursor);
-
-            switch(visit_day_type) {
-                case WEEKDAYS:  weekdays.add(result);   break;
-                case SATURDAY:  saturday.add(result);   break;
-                case SUNDAY:    sunday.add(result);     break;
-                default:        throw new java.lang.Error("Bad visit type");
-            }
-            num_results++;
-            cursor.moveToNext();
-        }
-        Log.d("TransperthCached", num_results + " results");
-
-        if (num_results == 0) {
-            // ie, no such stop
-            return null;
-        }
-
-        StopTimetable st = new StopTimetable(
-            stop_num,
-
-            weekdays,
-            saturday,
-            sunday
-        );
         _cache.put(stop_num, st);
         Log.d("TransperthCached", "cached");
 
