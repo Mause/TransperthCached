f61349af26d0ae96ab5be67ed6bb7898eac278d7
Dominic May <jack.thatch@gmail.com>
2015-07-24 18:58:48 +0800

    Entrypoint to train flow

--- src/com/lysdev/transperthcached/activities/TrainActivity.java
+++ src/com/lysdev/transperthcached/activities/TrainActivity.java
@@ -1,15 +1,32 @@
 package com.lysdev.transperthcached.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.lysdev.transperthcached.activities.train.TrainLineSelectActivity;
+import com.lysdev.transperthcached.activities.train.Direction;
+import com.lysdev.transperthcached.R;
 
 public class TrainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.trains);
+    }
 
-        TextView textview = new TextView(this);
-        textview.setText("This is the train tab");
-        setContentView(textview);
+    public void onClickDirection(View viewery) {
+        Button button = (Button)viewery;
+        Direction dir = (
+            button.getId() == R.id.from_perth ?
+            Direction.FROM :
+            Direction.TO
+        );
+
+        Intent intent = new Intent(this, TrainLineSelectActivity.class);
+        intent.putExtra("direction", dir.ordinal());
+        startActivity(intent);
     }
 }
