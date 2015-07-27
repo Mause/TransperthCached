d888847f849a9da6573c9dcf09b78fad3541c660
Dominic May <jack.thatch@gmail.com>
2015-07-28 01:01:49 +0800

    Btw, move to using scala.xml

--- src/com/lysdev/transperthcached/livetimes/GetTimesForStation.scala
+++ src/com/lysdev/transperthcached/livetimes/GetTimesForStation.scala
@@ -2,20 +2,8 @@ package com.lysdev.transperthcached.livetimes
 
 import android.util.Log
 
-import java.util.ArrayList
-import java.util.HashMap
-import java.util.List
-
-import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
-
-import org.w3c.dom.Document
-import org.w3c.dom.Element
-import org.w3c.dom.Node
-import org.w3c.dom.NodeList
-
 import scala.collection.JavaConverters._
-import scala.collection.JavaConversions._
 
 object GetTimesForStation {
     def getTimes(station_name: String) : TimesForStation = {
@@ -27,24 +15,20 @@ object GetTimesForStation {
             return null
         }
 
-        val wrapper = new GetWrapper(doc.getDocumentElement())
-
-        val s_last_update = wrapper.get("LastUpdate")
+        val s_last_update = (doc \ "LastUpdate").text
         val dt = ISODateTimeFormat.dateHourMinuteSecond().parseDateTime(
             s_last_update
         )
 
-        val nodeList = (
-            doc.getElementsByTagName("Trips")
-            .item(0)
-            .asInstanceOf[Element]
-            .getElementsByTagName("SercoTrip")
+        val tripList = (
+            (doc \ "Trips" \ "SercoTrip")
+            .map(Trip.fromRaw(_))
+            .toList
+            .asJava
         )
 
-        val tripList = new NodeListIterator(nodeList).asScala.toSeq.map(Trip.fromNode(_))
-
         return new TimesForStation(
-            wrapper.get("Name"),
+            (doc \ "Name").text,
             dt,
             tripList
         )
