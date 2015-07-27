675f6a83ca5ae0bccd11805d5bdff4a5bb97494b
Dominic May <jack.thatch@gmail.com>
2015-07-28 01:36:13 +0800

    Port GetTimesForPlatform

---
+++ src/com/lysdev/transperthcached/livetimes/GetTimesForPlatform.scala
@@ -0,0 +1,35 @@
+package com.lysdev.transperthcached.livetimes
+
+import java.util.regex.Pattern
+import scala.collection.JavaConverters._
+
+object GetTimesForPlatform {
+    lazy val patt = Pattern.compile("[A-Z]{3}\\d")
+
+    def isValidCode(code: String) = patt.matcher(code).matches()
+
+    def getTimes(code: String) : TimesForPlatform = {
+        if (!isValidCode(code)) {
+            throw new InvalidPlatformCodeException()
+        }
+
+        val doc = Util.getXML(
+            "/GetTimesForPlatform", Seq("code" -> code).toMap
+        )
+
+        val trains = (
+            (doc \ "Trip")
+            .map(Trip.fromRaw(_))
+            .toList
+            .asJava
+        )
+
+        new TimesForPlatform(
+            Integer.parseInt((doc \ "Number").text.trim()),
+            (doc \ "Code").text,
+            (doc \ "StopNumber").text,
+            (doc \ "Position").text,
+            trains
+        )
+    }
+}
