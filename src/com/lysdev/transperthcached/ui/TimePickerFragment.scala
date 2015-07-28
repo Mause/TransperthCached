2d2436d96f209ffad7b6821c58e8f7c1bfd0bc65
Dominic May <jack.thatch@gmail.com>
2015-07-29 01:25:46 +0800

    Port TimePickerFragment

---
+++ src/com/lysdev/transperthcached/ui/TimePickerFragment.scala
@@ -0,0 +1,26 @@
+package com.lysdev.transperthcached.ui
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.text.format.DateFormat
+
+import org.joda.time.LocalTime
+
+
+class TimePickerFragment(listener : TimePickerDialog.OnTimeSetListener
+                         default_time : LocalTime)
+                        extends DialogFragment {
+
+    override
+    def onCreateDialog(savedInstanceState: Bundle) : Dialog = {
+        new CustomTimePickerDialog(
+            getActivity(),
+            listener,
+            this.default_time.getHourOfDay(),
+            this.default_time.getMinuteOfHour(),
+            DateFormat.is24HourFormat(getActivity())
+        )
+    }
+}
